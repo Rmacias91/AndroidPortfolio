793f1514898fd9f76517a4ffb22a1304a95e1f1b
Richard Macias <Rmacias2454@gmail.com>
2016-07-09 10:29:08 -0500

    Criminal Intent App Started

---
+++ BNRportfolio/CriminalIntent/app/src/main/java/com/example/richie/criminalintent/Crime.java
@@ -0,0 +1,48 @@
+package com.example.richie.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by Richie on 7/9/2016.
+ */
+public class Crime {
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public Crime(){
+        mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+
+}
