18d39de6254f063aab82d323931fe1d9d36bf15d
Richard Macias <richardmacias@Ampied-I-Mac-0028.local>
2016-11-04 11:09:53 -0500

    Working on detail stillll

--- Movies/app/src/main/java/com/richie_ee/movies/DetailMovieActivity.java
+++ Movies/app/src/main/java/com/richie_ee/movies/DetailMovieActivity.java
@@ -4,21 +4,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by Richie on 10/30/2016.
  */
 public class DetailMovieActivity extends AppCompatActivity {
 
-    public DetailMovieActivity(){}
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_movie);
+        setContentView(R.layout.movie_container);
         if(savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new DetailMovieFragment()
+                    .add(R.id.container, new DetailMovieFragment())
                     .commit();
         }
 
@@ -38,7 +40,18 @@ public class DetailMovieActivity extends AppCompatActivity {
     public static class DetailMovieFragment extends Fragment{
 
 
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
 
+            View rootView = inflater.inflate(R.layout.movie_detail, container, false);
+
+            Bundle data = getActivity().getIntent().getExtras();
+            Movie movie = (Movie) data.getParcelable("student");
+
+
+            return rootView;
+        }
 
     }
 
--- Movies/app/src/main/java/com/richie_ee/movies/MainMovieActivity.java
+++ Movies/app/src/main/java/com/richie_ee/movies/MainMovieActivity.java
@@ -1,31 +1,14 @@
 package com.richie_ee.movies;
 
-import android.content.Context;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.ImageView;
-import android.widget.ListView;
-
-import com.squareup.picasso.Picasso;
-
-import java.util.List;
 
 public class MainMovieActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_movie);
+        setContentView(R.layout.movie_container);
         if(savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new MainMovieFragment())
